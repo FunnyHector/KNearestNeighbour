85673472dd8c734a1d75da2561f79c43b6d66e6b
Hector Zhao <hector.zhao@powershop.co.nz>
2017-03-21 20:25:31 +1300

    fix k-value string-integer conversion bug

--- main.rb
+++ main.rb
@@ -7,7 +7,7 @@ DEFAULT_K_VALUE           = 1
 
 training_set_file = ARGV[0].nil? ? DEFAULT_TRAINING_SET_FILE : ARGV[0]
 test_set_file = ARGV[1].nil? ? DEFAULT_TEST_SET_FILE : ARGV[1]
-k_value = ARGV[2].nil? ? DEFAULT_K_VALUE : ARGV[2]
+k_value = ARGV[2].nil? ? DEFAULT_K_VALUE : ARGV[2].to_i
 
 classifier = KNearestNeighbourClassifier.new(training_set_file, test_set_file, k_value)
 
